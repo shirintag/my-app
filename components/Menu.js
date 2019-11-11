e59b21a80ffb96c8c77d8dd52deca3b8b4789659
Shirin Taghavi <shirintaghavi@Shirins-MacBook-Pro.local>
2019-11-11 11:55:57 +0100

    Try to use react-native-paper components

---
+++ components/Menu.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Paragraph, Menu, Divider, Provider } from 'react-native-paper';
+
+
+export default class MenuView extends React.Component {
+    state = {
+        visible: false,
+    };
+
+    _openMenu = () => this.setState({ visible: true });
+
+    _closeMenu = () => this.setState({ visible: false });
+
+    render() {
+        return (
+            <Provider>
+                <View
+                    style={{
+                        paddingTop: 50,
+                        height: '100%',
+                        flexDirection: 'row'
+                    }}>
+                    <Menu
+                        style={{
+                            height: '100%',
+                            width: '80%',
+                        }}
+                        visible={this.state.visible}
+                        onDismiss={this._closeMenu}
+                        anchor={
+                            <Button onPress={this._openMenu}>Show menu</Button>
+                        }
+                    >
+                        <Menu.Item onPress={() => { }} title="Account" />
+                        <Menu.Item onPress={() => { }} title="Driver's Guide" />
+                        <Divider />
+                        <Menu.Item onPress={() => { }} title="FAQ" />
+                        <Menu.Item onPress={() => { }} title="Support" />
+                    </Menu>
+                </View>
+            </Provider>
+        );
+    }
+}
